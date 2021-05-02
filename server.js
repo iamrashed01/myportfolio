0e80cf062b58e0c1dc25126d1bd20695852b1e79
iamrashed01 <irashad42@gmail.com>
2021-05-02 23:05:43 +0600

    FEATE: add prod config for client

--- server.js
+++ server.js
@@ -16,6 +16,13 @@ app.use(express.urlencoded({ extended: true }));
 // routes
 app.use("/api/auth", require("./routes/auth"));
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
 app.use((req, res, next) => {
   res.json({ message: "Route not found", success: false });
 });
