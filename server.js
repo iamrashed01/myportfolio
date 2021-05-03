8e86f7b089b882ea1ced900cb3b727dc133cf465
iamrashed01 <irashad42@gmail.com>
2021-05-03 18:38:34 +0600

    FIX: upload as public, multer upload redo if err

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 require("express-async-errors");
 const morgan = require("morgan");
 require("dotenv").config();
+const fs = require("fs");
 
 // create app
 const app = express();
@@ -12,6 +13,7 @@ app.use(cors());
 app.use(morgan("tiny"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/uploads", express.static("uploads"));
 
 // routes
 app.use("/api/auth", require("./routes/auth"));
@@ -28,15 +30,21 @@ app.use((req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  if (err.message === "access denied") {
+  if (req.file && req.file.path) {
+    const isExist = fs.existsSync(req.file.path);
+    if (isExist) {
+      fs.unlinkSync(req.file.path);
+    }
+  }
+  if (err._original) {
+    return res.status(403).json({ message: err.message });
+  } else if (err.message === "access denied") {
     return res.status(403).json({ message: err.message });
   } else if (err.message) {
-    return res.status(403).json({ message: err.message });
+    return res.status(500).json({ message: err.message });
   } else {
-    return res.status(403).json({ message: "Something went wrong!" });
+    return res.status(500).json({ message: "Something went wrong!" });
   }
-
-  next(err);
 });
 
 // init server listener
