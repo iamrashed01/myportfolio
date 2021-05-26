f52b932cd7f27fdb3aca306b44133bd0d3a8e08f
iamrashed01 <irashad42@gmail.com>
2021-05-27 02:17:38 +0600

    dd

--- server.js
+++ server.js
@@ -15,19 +15,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/uploads", express.static("uploads"));
 
-// routes
-app.use("/api/welcome", require("./routes/welcome"));
-app.use("/api/auth", require("./routes/auth"));
-app.use("/api/profile", require("./routes/user"));
-app.use("/api/project", require("./routes/project"));
-app.use("/api", require("./routes/admin"));
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
+
+// routes
+app.use("/api/welcome", require("./routes/welcome"));
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/profile", require("./routes/user"));
+app.use("/api/project", require("./routes/project"));
+app.use("/api", require("./routes/admin"));
 
 app.use((req, res, next) => {
   res.json({ message: "Route not found", success: false });
